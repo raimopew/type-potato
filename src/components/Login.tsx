9d7a9b9fe75ff18db398f8c5f146439fbdd5c9f1
raimopew <raimo.pregel@gmail.com>
2024-09-16 16:44:49 +0300

    refactor: introduce different states

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,21 +2,44 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { FormEvent, useState, useEffect } from "react";
 
 export default function MyForm() {
-  // const [form, setForm] = useState({ username: "", password: "" });
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({ username: "", password: "" });
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    // event.preventDefault();
-    // console.log(form);
+    event.preventDefault();
+
+    console.log(form);
+
+    // console.log({
+    //   username,
+    //   password,
+    // });
   };
 
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setForm((prevForm) => ({
-  //     ...prevForm,
-  //     [event.target.name]: event.target.value,
-  //   }));
-  // };
+  // useEffect(() => {
+  //   console.log("Renders every time");
+  // });
+
+  // useEffect(() => {
+  //   console.log("Renders first time");
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log("Renders every time username changes");
+  // }, [username]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // setForm(prevForm => {
+    //   return {
+    //   }
+    // })
+
+    setForm((prevForm) => ({
+      ...prevForm,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   return (
     <Box
@@ -39,17 +62,15 @@ export default function MyForm() {
             name="username"
             label="Username"
             variant="outlined"
-            onChange={(event) => setUsername(event.target.value)}
-            // onChange={handleChange}
-            // value={form.username}
+            // onChange={(event) => setUsername(event.target.value)}
+            onChange={handleChange}
           />
           <TextField
             name="password"
             label="Password"
             variant="outlined"
-            onChange={(event) => setPassword(event.target.value)}
-            // onChange={handleChange}
-            // value={form.password}
+            // onChange={(event) => setPassword(event.target.value)}
+            onChange={handleChange}
           />
           <Button variant="contained" type="submit">
             Login
