1d492f47a0dc06de4102b15ea5cd9e47cd80ffc3
raimopew <raimo.pregel@gmail.com>
2024-09-23 13:28:54 +0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import "./App.css";
-import MyContainer from "./layout/MyContainer";
-import Login from "./components/Login";
-import { Outlet } from "react-router-dom";
+import "./App.css"
+import MyContainer from "./layout/MyContainer"
+import Login from "./components/Login"
+import { Outlet, useLocation } from "react-router-dom"
+import Header from "./layout/Header"
+import { Typography } from "@mui/material"
 
 function App() {
+  let location = useLocation()
+
   return (
     <>
-      <h1>Header</h1>
-      <Outlet />
+      <Header />
+      <MyContainer>
+        <Outlet />
+      </MyContainer>
+      {location?.pathname === "/" && (
+        <MyContainer>
+          <Typography variant="h4">Welcome to my humble abode</Typography>
+        </MyContainer>
+      )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
