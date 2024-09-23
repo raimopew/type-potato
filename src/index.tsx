2347fb094f4aaea5b0a992367c65934c5a4c6cf0
raimopew <raimo.pregel@gmail.com>
2024-09-23 13:33:24 +0300

    refactor: replace BrowserRouter w HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import reportWebVitals from "./reportWebVitals"
 
 import {
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import MyForm from "./components/Login";
+} from "react-router-dom"
+import App from "./App"
+import MyForm from "./components/Login"
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/form" element={<MyForm />} />
-      <Route path="/extra" element={<h1>Extra</h1>} />
-      <Route path="/contact" element={<h1>Contact</h1>} />
-    </Route>
-  )
-);
+    <Route
+      path="/"
+      element={<App />}
+    >
+      <Route
+        path="/form"
+        element={<MyForm />}
+      />
+      <Route
+        path="/extra"
+        element={<h1>Extra</h1>}
+      />
+      <Route
+        path="/contact"
+        element={<h1>Contact</h1>}
+      />
+    </Route>,
+  ),
+)
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
